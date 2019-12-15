43e2c1aa39b26cc289def1d99fc84a2855935a0c
Gruintjes <GruintjesTEJ@rabobank.corp>
2019-12-15 21:42:47 +0100

    ALT

--- src/pages/ionic/Chips.svelte
+++ src/pages/ionic/Chips.svelte
@@ -27,6 +27,7 @@
     <ion-chip>
       <ion-avatar>
         <img
+          alt="avatar"
           src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA1MTIgNTEyIj48cGF0aCBmaWxsPSIjYzVkYmZmIiBkPSJNMCAwaDUxMnY1MTJIMHoiLz48cGF0aCBkPSJNMjU2IDMwNGM2MS42IDAgMTEyLTUwLjQgMTEyLTExMlMzMTcuNiA4MCAyNTYgODBzLTExMiA1MC40LTExMiAxMTIgNTAuNCAxMTIgMTEyIDExMnptMCA0MGMtNzQuMiAwLTIyNCAzNy44LTIyNCAxMTJ2NTZoNDQ4di01NmMwLTc0LjItMTQ5LjgtMTEyLTIyNC0xMTJ6IiBmaWxsPSIjODJhZWZmIi8+PC9zdmc+" />
       </ion-avatar>
       <ion-label>Avatar</ion-label>
