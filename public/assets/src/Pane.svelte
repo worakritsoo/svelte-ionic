354c29f84287fd48d21882051f2ad105037eb9bc
Gruintjes <GruintjesTEJ@rabobank.corp>
2020-01-09 15:54:20 +0100

    Pane update

--- public/assets/src/Pane.svelte
+++ public/assets/src/Pane.svelte
@@ -59,49 +59,49 @@
     </a>
 
   </ion-text>
+
+  <div class="cupertino-pane">
+    <div class="cupertino-header">
+      <h1>Drag me up!</h1>
+
+    </div>
+    <div class="cupertino-content">
+      <ion-text color="primary">
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+      </ion-text>
+
+      <ion-text color="secondary">
+        <h2>Nam rutrum justo massa, maximus elementum leo dignissim ac.</h2>
+      </ion-text>
+
+      <ion-text color="tertiary">
+        <h3>Vestibulum eleifend lorem nec neque interdum varius.</h3>
+      </ion-text>
+
+      <ion-text color="success">
+        <h4>Sed in neque at nibh congue tincidunt.</h4>
+      </ion-text>
+
+      <ion-text color="warning">
+        <h5>
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia Curae;
+        </h5>
+      </ion-text>
+
+      <ion-text color="danger">
+        <h6>Suspendisse potenti.</h6>
+      </ion-text>
+
+      <p>
+        Donec magna odio,
+        <ion-text color="primary">semper</ion-text>
+        ac nibh et, vestibulum eleifend felis. Donec
+        <ion-text color="secondary">pulvinar</ion-text>
+        ex non quam vulputate malesuada in a magna. Praesent massa arcu,
+        <ion-text color="tertiary">vehicula</ion-text>
+        id pharetra et, cursus at lectus.
+      </p>
+    </div>
+  </div>
 </ion-content>
-
-<div class="cupertino-pane">
-  <div class="cupertino-header">
-    <h1>Drag me up!</h1>
-
-  </div>
-  <div class="cupertino-content">
-    <ion-text color="primary">
-      <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
-    </ion-text>
-
-    <ion-text color="secondary">
-      <h2>Nam rutrum justo massa, maximus elementum leo dignissim ac.</h2>
-    </ion-text>
-
-    <ion-text color="tertiary">
-      <h3>Vestibulum eleifend lorem nec neque interdum varius.</h3>
-    </ion-text>
-
-    <ion-text color="success">
-      <h4>Sed in neque at nibh congue tincidunt.</h4>
-    </ion-text>
-
-    <ion-text color="warning">
-      <h5>
-        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-        cubilia Curae;
-      </h5>
-    </ion-text>
-
-    <ion-text color="danger">
-      <h6>Suspendisse potenti.</h6>
-    </ion-text>
-
-    <p>
-      Donec magna odio,
-      <ion-text color="primary">semper</ion-text>
-      ac nibh et, vestibulum eleifend felis. Donec
-      <ion-text color="secondary">pulvinar</ion-text>
-      ex non quam vulputate malesuada in a magna. Praesent massa arcu,
-      <ion-text color="tertiary">vehicula</ion-text>
-      id pharetra et, cursus at lectus.
-    </p>
-  </div>
-</div>
