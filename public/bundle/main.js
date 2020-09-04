d6dd8825a58ae1528f0340be61ca72093b809df8
Gruintjes <GruintjesTEJ@rabobank.corp>
2020-09-04 22:29:35 +0200

    SMall things

--- public/bundle/main.js
+++ public/bundle/main.js
@@ -1,4 +1,4 @@
 
 (function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { W as default } from './main-e8d27d69.js';
+export { W as default } from './main-34572ad4.js';
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import App from './App.svelte';
 
-import { myRoutes as routes } from "./myRoutes.js";
+// import { myRoutes as routes } from "./myRoutes.js";
+
+import { routes } from "@sveltech/routify/tmp/routes";
 
 
 const app = new App({
