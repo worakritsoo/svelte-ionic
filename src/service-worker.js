14ac8597bc64bed186b43efa8fcdfccd04047c59
Tom Gruintjes <tom.gruintjes@gmail.com>
2021-02-21 20:49:49 +0100

    Some more work

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,31 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const cacheName = 'stale-with-revalidate'
-
-const { routing, strategies } = workbox
-
 console.log('Hello from service-worker.js!');
 
 if (workbox) {
-    console.log(`Yay! Workbox is loaded 🎉`, routing);
+    console.log(`Yay! Workbox is loaded 🎉`, workbox.routing);
 } else {
     console.log(`Boo! Workbox didn't load 😬`);
 }
-
-// implements staleWhileRevalidate to all routes
-routing.registerRoute(
-    () => true,
-    new strategies.StaleWhileRevalidate({ cacheName }),
-)
-
-// removes all caches not named <cacheName>
-const invalidateOldCache = async() => {
-    const keys = await caches.keys()
-    const isOldCache = (key) => key !== cacheName
-    const oldKeys = keys.filter(isOldCache)
-
-    return Promise.all(oldKeys.map((key) => caches.delete(key)))
-}
-
-// runs invalidateOldCache on activation
-self.addEventListener('activate', (e) => e.waitUntil(invalidateOldCache()))
